8b6138dc1fdb7e493fbafe0f517c41259685d988
WCoDesigns <joanne@watsonci.com.au>
2026-03-23 15:45:56 +1100

    Create HomePage.js

---
+++ src/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial", background: "#F7F5F3" }}>
+      <h1>W&Co Designs Pty Ltd</h1>
+      <p>Premium Australian-Made Commercial Furniture</p>
+    </main>
+  );
+}
